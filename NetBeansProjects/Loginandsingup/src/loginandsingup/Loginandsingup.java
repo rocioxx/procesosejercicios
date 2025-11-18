1dc7abeef0e459f4cf7bcded0a7c5aa88c3d00a4
rocioparra678 <rocioparra678@gmail.com>
2025-11-18 12:26:14 +0100

    subida de login

---
+++ NetBeansProjects/Loginandsingup/src/loginandsingup/Loginandsingup.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loginandsingup;
+
+/**
+ *
+ * @author rocio
+ */
+public class Loginandsingup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         login Loginframe = new login();
+             Loginframe. setVisible(true);
+            Loginframe.pack();
+            Loginframe.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ NetBeansProjects/probandointerfaces/src/main/java/com/mycompany/probandointerfaces/Probandointerfaces.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.probandointerfaces;
+
+/**
+ *
+ * @author rocio
+ */
+public class Probandointerfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
