29c2924f08a34014c8a9ba3820eea1d9d94ba7b8
rocioparra678 <rocioparra678@gmail.com>
2025-11-19 09:43:29 +0100

    .

--- NetBeansProjects/LoginApp/src/javaapplication6/JavaApplication6.java
+++ NetBeansProjects/LoginApp/src/javaapplication6/JavaApplication6.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package loginapp;
+package javaapplication6;
 
 /**
  *
  * @author rocio
  */
-public class LoginApp {
+public class JavaApplication6 {
 
     /**
      * @param args the command line arguments
