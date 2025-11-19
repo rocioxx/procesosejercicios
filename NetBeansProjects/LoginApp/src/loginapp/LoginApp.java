11d64ef7d8bffc06861e857c1e1b4c3e32caf5cf
rocioparra678 <rocioparra678@gmail.com>
2025-11-19 17:10:09 +0100

    Retry login

--- NetBeansProjects/LoginApp/src/loginapp/LoginApp.java
+++ NetBeansProjects/LoginApp/src/loginapp/LoginApp.java
@@ -1,10 +1,8 @@
-package javaapplication6;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
+package loginapp;
 
 /**
  *
